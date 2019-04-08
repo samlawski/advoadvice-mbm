0a61a7c271f9bff848d36d450860c3ec026a739e
Pawel Cofta <pawel@cofta.de>
2019-04-08 10:34:17 +0700

    file upload form

--- _assets/javascripts/kreditewiderrufen.js
+++ _assets/javascripts/kreditewiderrufen.js
@@ -3,26 +3,26 @@ $( 'form#kreditwiderrufen' ).submit(function ( e ) {
 
     var form = document.querySelector("#kreditwiderrufen");
     var formdata = new FormData(form);
-    // var url = 'http://localhost:3000/api/contacts';
-    var url = 'http://staging-auto.kanzlei-fuer-widerruf.de/api/contacts';
+    var url = 'http://localhost:3000/api/contacts';
+    // var url = 'http://staging-auto.kanzlei-fuer-widerruf.de/api/contacts';
     var documents = document.querySelector("#documents");
     var files = documents.files
     for (var i = 0; i < files.length; i++) {
         formdata.append('contact[documents][]', files[i], files[i]['name']);
     }
    
-    // formdata.append('contact[documents]', documents.files[0], 'filename.txt');
-
     fetch(url, {
         method: "POST",
         // headers: { 'Content-Type':'multipart/form-data' },
         body: formdata,
-    }).then(
-        response => response.text() // .json(), etc.
-        // same as function(response) {return response.text();}
-    ).then(
-        html => {
-            console.log(html)
-        }
-    );
+    }).then(function(resp){
+        console.log(resp.status);
+        return resp.json();
+    })
+    .then(function(data) {
+        console.log(data);        
+    })
+    .catch(function(error) {
+        console.log(error);
+    });   
 });
