6ccaa0b873ac6e2b9b8d53eafad604df106b5c41
Pawel Cofta <pawel@cofta.de>
2019-04-11 15:55:34 +0700

    kreditwiderrufen form api url

--- _assets/javascripts/kreditewiderrufen.js
+++ _assets/javascripts/kreditewiderrufen.js
@@ -5,8 +5,8 @@ $( 'form#kreditwiderrufen' ).submit(function ( e ) {
 
     var form = document.querySelector("#kreditwiderrufen");
     var formdata = new FormData(form);
-    var url = 'http://localhost:3000/api/contacts';
-    // var url = 'http://staging-auto.kanzlei-fuer-widerruf.de/api/contacts';
+    // var url = 'http://localhost:3000/api/contacts';
+    var url = 'https://auto.kanzlei-fuer-widerruf.de/api/contacts';
     var documents = document.querySelector("#documents");
     var files = documents.files
     for (var i = 0; i < files.length; i++) {
