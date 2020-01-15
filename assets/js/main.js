e0e1893f853894be8cedeb9bd8cb1788b16838fb
Sam <sam.bogu@gmail.com>
2020-01-15 09:49:15 +0100

    clicking links closes nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,9 @@ $('.hamburger').click(function(){
   $(this).toggleClass('is-active')
   $('.nav__menu').toggleClass('open')
 })
+// Link Clicks should also close the menu
+$('.nav__menu a').click(function(){
+  $('.hamburger').removeClass('is-active')
+  $('.nav__menu').removeClass('open')
+})
 
