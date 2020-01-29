9f96642fb69b96eba1a94b7dcacd940f795e88ea
samlawski <sam@broskiBook19.local>
2020-01-29 10:30:45 +0100

    replace jquery with vanilla

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,22 @@
-// Navigation
+/* *** Navigation *** */
 
-$('.hamburger').click(function(){
-  $(this).toggleClass('is-active')
-  $('.nav__menu').toggleClass('open')
-})
-// Link Clicks should also close the menu
-$('.nav__menu a').click(function(){
-  $('.hamburger').removeClass('is-active')
-  $('.nav__menu').removeClass('open')
+var $navBtn   = document.querySelector('.hamburger'),
+    $navMenu  = document.querySelector('.nav__menu'),
+    $navLinks = document.querySelectorAll('.nav__menu a')
+
+// Toggle Hamburger menu
+$navBtn.onclick = function(){
+  $navBtn.classList.toggle('is-active')
+  $navMenu.classList.toggle('open')
+}
+
+// Close menu when clicking links
+$navLinks.forEach(function($link){
+  $link.addEventListener('click', function(){
+    $navBtn.classList.remove('is-active')
+    $navMenu.classList.remove('open')
+  })
 })
 
+
+/* *** Search *** */
