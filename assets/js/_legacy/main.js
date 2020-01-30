38ccb40fa98f510d851845272299870f9a70d2ff
samlawski <sam@broskiBook19.local>
2020-01-30 12:36:46 +0100

    delete large round of legacy files

--- assets/js/_legacy/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  layout.init();
-  search.init();
-  cookies.init();
-
-  if($('.schufaTool').length > 0) schufaTool.init();
-  ($('.landing__header').length >= 1) ? landing.init() : false;
-});
